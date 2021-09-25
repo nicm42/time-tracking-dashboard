056f234c4d8cfd0706d776d7bc6eb52dc12b9082
Nic <42087659+nicm42@users.noreply.github.com>
2021-09-25 13:41:10 +0100

    Set it to weekly numbers from data json by default

--- src/main.ts
+++ src/main.ts
@@ -16,23 +16,30 @@ const titles = document.querySelectorAll('.stats__title');
 const hours = document.querySelectorAll('.stats__number');
 const last = document.querySelectorAll('.stats__previous');
 
+const setData = (time: string): void => {
+  // Change eg weekly to eg Week
+  let timeText = time.charAt(0).toUpperCase() + time.slice(1, -2);
+  // However for daily this have Dai, but we want Day
+  // So we'll need to do that separately
+  if (time === 'daily') {
+    timeText = 'Day';
+  }
+
+  data.forEach((element, index) => {
+    const times = element.timeframes[time as keyof ITimes];
+    const current = times.current;
+    const previous = times.previous;
+    titles[index].innerHTML = element.title;
+    hours[index].innerHTML = `${current}hrs`;
+    last[index].innerHTML = `Last ${timeText} - ${previous}hrs`;
+  });
+};
+
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
-    const time = button.id; // eg weekly
-    let timeText = time.charAt(0).toUpperCase() + time.slice(1, -2); // eg Week
-    // However for daily this have Dai, but we want Day
-    // So we'll need to do that separately
-    if (time === 'daily') {
-      timeText = 'Day';
-    }
-
-    data.forEach((element, index) => {
-      const times = element.timeframes[time as keyof ITimes];
-      const current = times.current;
-      const previous = times.previous;
-      titles[index].innerHTML = element.title;
-      hours[index].innerHTML = `${current}hrs`;
-      last[index].innerHTML = `Last ${timeText} - ${previous}hrs`;
-    });
+    setData(button.id);
   });
 });
+
+// Set it to default to weekly if no button has been pressed yet
+setData('weekly');
