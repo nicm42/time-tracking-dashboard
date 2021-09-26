ca80b9c94647b26485b692bcff968814f0e9e50a
Nic <42087659+nicm42@users.noreply.github.com>
2021-09-26 18:23:01 +0100

    Added initial state tests

--- cypress/integration/page.test.ts
+++ cypress/integration/page.test.ts
@@ -8,5 +8,20 @@ describe('Page tests', () => {
   it('Tests the page loads with the weekly numbers', () => {
     cy.get('.work').contains('32hrs');
     cy.get('.work').contains('Last Week - 36hrs');
+
+    cy.get('.play').contains('10hrs');
+    cy.get('.play').contains('Last Week - 8hrs');
+
+    cy.get('.study').contains('4hrs');
+    cy.get('.study').contains('Last Week - 7hrs');
+
+    cy.get('.exercise').contains('4hrs');
+    cy.get('.exercise').contains('Last Week - 5hrs');
+
+    cy.get('.social').contains('5hrs');
+    cy.get('.social').contains('Last Week - 10hrs');
+
+    cy.get('.self-care').contains('2hrs');
+    cy.get('.self-care').contains('Last Week - 2hrs');
   });
 });
